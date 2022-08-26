912af4c96e08b61ce2c4748893f50b80ff8d0dd9
Leo Unglaub <leo@unglaub.at>
2022-08-27 01:19:24 +0200

    Pass the correct params down to all other functions (self mixup)
    This also fixed #1 where `!keys` chat messages where not correctly handled.
    
    Signed-off-by: Leo Unglaub <leo@unglaub.at>

--- src/handler-events.lua
+++ src/handler-events.lua
@@ -4,30 +4,30 @@ KeystoneReporter.HandlerEvents.eventFrame = CreateFrame("EventFrame")
 
 -- Reply to "!keys" and "!key" chat messages in a party. This responds only yo the
 -- current party itself.
-KeystoneReporter.HandlerEvents.ReplyToKey = function (text)
+KeystoneReporter.HandlerEvents.ReplyToKey = function(text)
 	if text == "!keys" or text == "!key" then
-		KeystoneReporter.Sender.party()	
+		KeystoneReporter.Sender.party()
 	end
 end
 
 -- A catch-all handler for all registered events
-KeystoneReporter.HandlerEvents.eventFrame.MassEventHandler = function (self, event, ...)
-	self[event](self, event, ...)
+KeystoneReporter.HandlerEvents.eventFrame.MassEventHandler = function(self, event, ...)
+	self[event](...)
 end
 
 -- Event Handler: CHAT_MSG_PARTY
-KeystoneReporter.HandlerEvents.eventFrame.CHAT_MSG_PARTY = function (self, text)
+KeystoneReporter.HandlerEvents.eventFrame.CHAT_MSG_PARTY = function(text)
 	KeystoneReporter.HandlerEvents.ReplyToKey(text)
 end
 
 -- Event Handler: CHAT_MSG_PARTY_LEADER
-KeystoneReporter.HandlerEvents.eventFrame.CHAT_MSG_PARTY_LEADER = function (self, text)
+KeystoneReporter.HandlerEvents.eventFrame.CHAT_MSG_PARTY_LEADER = function(text)
 	KeystoneReporter.HandlerEvents.ReplyToKey(text)
 end
 
 
 -- Event Handler: CHAT_MSG_ADDON
-KeystoneReporter.HandlerEvents.eventFrame.CHAT_MSG_ADDON = function (self, text)
+KeystoneReporter.HandlerEvents.eventFrame.CHAT_MSG_ADDON = function(text)
 	-- stop if we are not interrested in the ADDON MSG
 	if prefix ~= "KeystoneReporter" then
 		return
